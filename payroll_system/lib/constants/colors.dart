b51bc29987412cefcec72e9b396b67e81dd23282
Reshma <chirag@omtecweb.com>
2022-12-22 15:58:30 +0530

    Appcolors updated

--- payroll_system/lib/constants/colors.dart
+++ payroll_system/lib/constants/colors.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color mainBackWhitegroundColor = Colors.white;
   static const Color blackColor = Colors.black;
-  static const Color whiteColor= Colors.white;
+  static const Color whiteColor = Colors.white;
   static const Color blueColor = Colors.blue;
   static const Color greyColor = Colors.grey;
-
+  static const Color redColor = Colors.red;
+  static const Color greenColor = Colors.green;
 }
