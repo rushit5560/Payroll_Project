c3af020403235f4760a5fafeff03cf38c0901a12
Rushit <rushit@omtecweb.com>
2022-12-22 15:49:23 +0530

    Update messaging.dart

--- payroll_system/lib/Utils/messaging.dart
+++ payroll_system/lib/Utils/messaging.dart
@@ -22,4 +22,40 @@ class AppMessage {
   static String adminName = "Admin";
   static String companiesName = "Companies";
   static String companyName = "Company Name";
+  static String phoneNoName = "Phone No";
+  static String verifiedStatusName = "Verified Status";
+  static String verifiedStatusValueName = "In=Active";
+  static String edit = 'Edit';
+  static String delete = 'Delete';
+  static String yes = 'Yes';
+  static String no = 'No';
+
+  static String deleteAlertMessage = 'Are you sure you want to delete ?';
+
+  //Employee
+  static String employeeName = 'Employee Name';
+  static String employeePhoneNumber = 'Phone Number';
+  static String employeeDepartmentName = 'Department Name';
+  static String employeeCompanyName = 'Company Name';
+  static String employeeStatus = 'Status';
+  static String employeeList = 'Employee List';
+  static String firstName = 'First Name';
+  static String middleName = 'Middle Name';
+  static String lastName = 'Last Name';
+  static String phoneNo = 'Phone No';
+  static String dateOfBrith = 'Date Of Birth';
+  static String department = 'Department';
+  static String homeNo = 'Home No';
+  static String workNO = 'Work No';
+  static String hourlyRate = 'HourlyRate';
+  static String salary = 'Salary';
+  static String startDate = 'Start Date';
+  static String lastDay = 'Last Day';
+  static String isActive = 'IsActive';
+  static String company = 'Company';
+  static String employeeEmail = 'Email';
+  static String employeePassword = 'Password';
+  static String employeeCurrentAddress = 'Current Address';
+  static String employeeHomAddresse = 'Home Address';
+  static String employeeDetails = 'Employee Details';
 }
