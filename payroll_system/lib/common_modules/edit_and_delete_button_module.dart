877ae455a96f0a8c2c495986a0e438356781cc0d
Rushit <rushit@omtecweb.com>
2022-12-22 15:25:57 +0530

    add common module file

---
+++ payroll_system/lib/common_modules/edit_and_delete_button_module.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:payroll_system/Utils/messaging.dart';
+import 'package:payroll_system/Utils/style.dart';
+import 'package:payroll_system/constants/colors.dart';
+import 'package:sizer/sizer.dart';
+
+class EditAndDeleteButtonModule extends StatelessWidget {
+  Function() onEditTap;
+  Function() onDeleteTap;
+
+  EditAndDeleteButtonModule(
+      {Key? key, required this.onEditTap, required this.onDeleteTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Expanded(
+          flex: 5,
+          child: ElevatedButton(
+            onPressed: onEditTap,
+            child: Text(
+              AppMessage.edit,
+              style: TextStyleConfig.textStyle(
+                fontSize: 15.sp,
+                textColor: AppColors.whiteColor,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(width: 4.w),
+        Expanded(
+          flex: 5,
+          child: ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(AppColors.redColor),
+            ),
+            onPressed: onDeleteTap,
+            child: Text(
+              AppMessage.delete,
+              style: TextStyleConfig.textStyle(
+                fontSize: 15.sp,
+                textColor: AppColors.whiteColor,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ payroll_system/lib/common_modules/single_item_module.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:payroll_system/Utils/extensions.dart';
+import 'package:payroll_system/Utils/style.dart';
+
+
+class SingleListTileCustom extends StatelessWidget {
+  final String textKey;
+  final String textValue;
+
+  const SingleListTileCustom(
+  {super.key, required this.textValue, required this.textKey});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            textKey,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyleConfig.textStyle(fontWeight: FontWeight.w600),
+          ),
+        ),
+        Expanded(
+          child: Text(
+            textValue,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyleConfig.textStyle(),
+          ),
+        ),
+      ],
+    ).commonSymmetricPadding(vertical: 2);
+  }
+}
