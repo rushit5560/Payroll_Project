811c06271316f1fed449313fe9fbe85efa0e4ca5
Reshma <reshma@omtecweb.com>
2023-02-07 15:47:22 +0530

    update date format

--- payroll_system/lib/controllers/general_settings_screen_controller.dart
+++ payroll_system/lib/controllers/general_settings_screen_controller.dart
@@ -1,16 +1,20 @@
 import 'dart:convert';
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:payroll_system/models/genral_setting_model/genral_setting_model.dart';
+import 'package:payroll_system/models/genral_setting_update_model/genral_setting_update_model.dart';
 import 'package:payroll_system/utils/api_url.dart';
 import 'package:http/http.dart' as http;
+import '../utils/extension_methods/user_preference.dart';
 
 class GeneralSettingsScreenController extends GetxController {
   RxBool isLoading = false.obs;
   RxBool isSuccessStatus = false.obs;
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  int userIdPrefs = 0;
+  UserPreference userPreference = UserPreference();
 
   List<String> generalSettingsOptionList = [
     "Choose Option",
@@ -26,6 +30,7 @@ class GeneralSettingsScreenController extends GetxController {
   RxString selectedValue = "Choose Option".obs;
 
   Future<void> getDateFormatFunction() async {
+    log("userIdPrefsuserIdPrefs:   $userIdPrefs");
     isLoading(true);
     String url = ApiUrl.getDateFormatApi;
     log('Get getDateFormatFunction Api Url :$url');
@@ -40,7 +45,7 @@ class GeneralSettingsScreenController extends GetxController {
         selectedValue.value = genralSettingModel.data;
         log("getDateFormatFunction :: ${selectedValue.value}");
       } else {
-        log('getAllCompanyFunction Else'); 
+        log('getAllCompanyFunction Else');
       }
     } catch (e) {
       log('getAllCompanyFunction Error :$e');
@@ -50,9 +55,53 @@ class GeneralSettingsScreenController extends GetxController {
     }
   }
 
+  Future<void> updateDateFormatFunction() async {
+    isLoading(true);
+    String url = ApiUrl.updateDateFormatApi;
+    log("updateDateFormatFunction Api Url : $url");
+    try {
+      var request = http.MultipartRequest('POST', Uri.parse(url));
+      request.fields['datetype'] = selectedValue.value;
+      request.fields['userid'] = "$userIdPrefs";
+      request.fields['id'] = "1";
+
+      var response = await request.send();
+
+      response.stream
+          .transform(const Utf8Decoder())
+          .transform(const LineSplitter())
+          .listen((value) async {
+        log('value Update updateDateFormatFunction: $value');
+
+        DateFormatUpdateModel dateFormatUpdateModel =
+            DateFormatUpdateModel.fromJson(json.decode(value));
+
+        isSuccessStatus = dateFormatUpdateModel.success.obs;
+
+        if (isSuccessStatus.value) {
+          Fluttertoast.showToast(msg: dateFormatUpdateModel.messege);
+          Get.back();
+        } else {
+          log("updateDateFormatFunction");
+        }
+      });
+    } catch (e) {
+      log('updateDateFormatFunction Error :$e');
+      rethrow;
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  getLoggedInUserIdFromPrefs() async {
+    userIdPrefs = await userPreference.getIntValueFromPrefs(
+        keyId: UserPreference.userIdKey);
+    await getDateFormatFunction();
+  }
+
   @override
   void onInit() {
-    getDateFormatFunction();
+    getLoggedInUserIdFromPrefs();
     super.onInit();
   }
 }
